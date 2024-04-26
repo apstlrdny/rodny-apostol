d3c94b2d2c313f1a1f387210d8dde16debe33403
Ronald <apostolrodny0307@gmail.com>
2024-04-26 15:51:45 +0800

    dom

--- dom-manipulation/basics.js
+++ dom-manipulation/basics.js
@@ -1,11 +1,21 @@
-
 function generatePassword() {
     const value = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
-    const len = 8
+    const len = 16
     let password = ""
     for (let i = 0; i < len; i++) {
         const randomNumber = Math.floor(Math.random() * value.length)
         password += value.charAt(randomNumber)
     }
     document.getElementById("passwordInput").value = password;
+    document.getElementById("generatePasswordBtn").disabled = true;
+}
+
+function checkEmailInput() {
+    const emailInput = document.getElementById("validationCustom03");
+    const generatePasswordBtn = document.getElementById("generatePasswordBtn");
+    if (emailInput.value.trim() !== "") { 
+        generatePasswordBtn.disabled = false; 
+    } else {
+        generatePasswordBtn.disabled = true; 
+    }
 }
